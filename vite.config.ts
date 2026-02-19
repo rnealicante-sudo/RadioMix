45ea36dd88684059c8dfe0d4c217f9d01a433bb1
rnealicante-sudo <rnealicante@gmail.com>
2026-02-19 19:27:46 +0100

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -2,24 +2,30 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-    // Carga las variables de entorno (.env en local, Secrets en GitHub)
+    // Carga las variables de entorno (.env local o los Secrets de GitHub)
     const env = loadEnv(mode, '.', '');
     
     return {
-      // IMPORTANTE: Sustituye 'revoxmix' por el nombre exacto de tu repositorio en GitHub
+      // Configuramos la base con el nombre de tu repositorio
       base: '/RadioMix/', 
+      
       server: {
         port: 3000,
         host: '0.0.0.0',
       },
+      
       plugins: [react()],
+      
       define: {
         // Esto permite que la app use la API KEY de Gemini de forma segura
         'process.env.GEMINI_API_KEY': JSON.stringify(env.VITE_GEMINI_API_KEY || env.GEMINI_API_KEY)
       },
+      
       resolve: {
         alias: {
+          // Permite usar el alias @ para referirse a la raíz
           '@': path.resolve(__dirname, '.'),
         }
       }
