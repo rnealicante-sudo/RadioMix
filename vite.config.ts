0e8f4cfba71563525e7055114f3b825a9f582199
rnealicante-sudo <rnealicante@gmail.com>
2026-02-19 19:20:20 +0100

    Change base URL from '/revoxmix/' to '/RadioMix/'

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => {
     
     return {
       // IMPORTANTE: Sustituye 'revoxmix' por el nombre exacto de tu repositorio en GitHub
-      base: '/revoxmix/', 
+      base: '/RadioMix/', 
       server: {
         port: 3000,
         host: '0.0.0.0',
