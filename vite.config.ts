cc68d7636149533c08f1b915e969a83452cea85f
rnealicante-sudo <rnealicante@gmail.com>
2026-02-23 14:51:26 +0100

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'url';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
@@ -12,12 +12,11 @@ export default defineConfig(({ mode }) => {
       },
       plugins: [react()],
       define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
       resolve: {
         alias: {
-          '@': path.resolve(__dirname, '.'),
+          '@': fileURLToPath(new URL('.', import.meta.url)),
         }
       }
     };
